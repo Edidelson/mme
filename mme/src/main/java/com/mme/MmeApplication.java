c16daa8aab973bbd2d8e9757b04d0ef3a63f652d
Edidelson <delso23@gmail.com>
2024-08-05 18:21:38 -0300

    Primeiro versão

---
+++ mme/src/main/java/com/mme/MmeApplication.java
@@ -0,0 +1,13 @@
+package com.mme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmeApplication.class, args);
+	}
+
+}
